a144c97a2ab0bbc3a59a6013a5ae865079a4ac98
Florian Perrot <contact@florian-perrot.fr>
2015-02-26 09:42:10 +0100

    Definir les libs et deps utilisé

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,19 @@
 require.config({
+	//Definir les libs utilisé
 	paths: {
 		jquery: '../../vendor/jquery/dist/jquery',
+		underscore: '../../vendor/underscore/underscore',
+		backbone: '../../vendor/backbone/backbone'
+	},
+	//Defini les deps non supporté par requireJs
+	shim: {
+		'backbone': {
+			deps: ['underscore', 'jquery'],
+			export: 'Backbone'
+		},
+		'underscore': {
+			export: '_'	
+		}
 	}
 });
 require(
