0c4bfa80fcca36d177707537e10e66b9951b8efd
Florian Perrot <florianperrot0@gmail.com>
2015-03-12 09:20:50 +0100

    Ajout des elements du template pour checkinlist

--- assets/js/views/checkin/lists.js
+++ assets/js/views/checkin/lists.js
@@ -2,11 +2,22 @@ define(
     ['jquery', 'underscore', 'backbone', 'collections/checkins'],
     function($,_,Backbone,CheckinCollection){
         var CheckinListView = Backbone.View.extend({
+
+            el: "#checkinlist",
+
+            template: '<h3>test template</h3>',
+
             render: function(){
+                console.log('CheckInListView Render');
+
+                var self = this;
+
                 checkinCollection = new CheckinCollection();
+
                 checkinCollection.fetch({
                     success: function( checkins ){
-                        console.log( checkins.models );
+                        console.log( self.$el );
+                        self.$el.html( self.template );
                     }
                 });
             }
